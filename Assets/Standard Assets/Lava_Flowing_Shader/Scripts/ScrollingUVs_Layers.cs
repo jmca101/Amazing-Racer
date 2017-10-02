c80a4aeeeb87d6e00576760fabf45f02574ff9ac
DESKTOP-82A48MT\jmca1 <jmca101@hotmail.com>
2017-10-02 10:39:08 -0400

    Amazing Racer with Step one Complete. Step Two: Landscape Complete No Extra Finish added. A.i. Attempt Added.(still Needs Work)

---
+++ Assets/Standard Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingUVs_Layers : MonoBehaviour 
+{
+	//public int materialIndex = 0;
+	public Vector3 uvAnimationRate = new Vector3( 1.0f, 0.0f, 0.0f );
+	public string textureName = "_MainTex";
+	
+	Vector3 uvOffset = Vector3.zero;
+	
+	void LateUpdate() 
+	{
+		uvOffset += ( uvAnimationRate * Time.deltaTime );
+		if( GetComponent<Renderer>().enabled )
+		{
+			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
+		}
+	}
+}
